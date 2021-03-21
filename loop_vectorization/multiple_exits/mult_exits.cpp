3f5ee4299c9a96191c10d00593ecb7070b3ba345
bogdan <lakatosh@phystech.edu>
2021-03-21 14:36:47 +0300

    checked multiple exits

---
+++ loop_vectorization/multiple_exits/mult_exits.cpp
@@ -0,0 +1,36 @@
+﻿#include <vector>
+#include <stdexcept>
+
+
+
+
+
+int f_cont(std::vector<int> vec, int val){
+    int sum = 0;
+    for(size_t i=0; i<vec.size(); i++){
+        if(vec[i]==val) continue;
+        sum += vec[i];
+    }
+    return sum;
+}
+
+
+
+
+int f_break(std::vector<int> vec, int val){
+    int sum = 0;
+    for(size_t i=0; i<vec.size(); i++){
+        if(vec[i]==val) break;
+        sum += vec[i];
+    }
+    return sum;
+}
+
+int f_throw(std::vector<int> vec, int val){
+    int sum = 0;
+    for(size_t i=0; i<vec.size(); i++){
+        if(vec[i]==val) throw std::invalid_argument("smth");
+        sum += vec[i];
+    }
+    return sum;
+}
