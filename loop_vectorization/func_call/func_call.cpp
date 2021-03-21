fd3d661521a440ed76fb71db6981aabe73327b08
bogdan <lakatosh@phystech.edu>
2021-03-21 12:31:42 +0300

    tested func call

--- loop_vectorization/func_call/func_call.cpp
+++ loop_vectorization/func_call/func_call.cpp
@@ -1,5 +1,26 @@
 ﻿#include <vector>
 
+int func_out(int lhs, int rhs) noexcept;
+
+inline int func_inl(int lhs, int rhs){
+    return lhs + rhs;
+}
 
 
 
+int call_inl(std::vector<int> vec){
+    int sum = 0;
+    for(size_t i=0; i<vec.size(); i++){
+        sum += func_inl(vec[i], vec[i]);
+    }
+    return sum;
+}
+
+
+int call_out(std::vector<int> vec, size_t size){
+    int sum = 0;
+    for(size_t i=0; i<size; i++){
+        sum += func_out(vec[i], vec[i]);
+    }
+    return sum;
+}
