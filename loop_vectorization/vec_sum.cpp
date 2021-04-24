19528fc58c4f676e777edec078470be5b870feb0
bogdan <lakatosh@phystech.edu>
2021-04-24 20:55:04 +0300

    checked sum nothing shocking

--- loop_vectorization/vec_sum.cpp
+++ loop_vectorization/vec_sum.cpp
@@ -9,52 +9,60 @@ std::mt19937 rnd(42);
 std::uniform_real_distribution<double> dist;
 
 
-double sum_together(const std::vector<double> v1, const std::vector<double> v2) {
+double sum_together(const std::vector<std::vector<double>>& v) {
     double sum = 0;
-    for(size_t i=0; i<v1.size(); i++) {
-        sum += v1[i] + v2[i];
+    for(size_t i=0; i<v.size(); i++) {
+        for(size_t j=0; j<v[0].size(); j++) {
+            sum += v[i][j];
+        }
     }
     return sum;
 }
 
 
 
-double sum_separately(const std::vector<double> v1, const std::vector<double> v2) {
-    double sum = std::accumulate(v1.begin(), v1.end(), 0.0);
-    sum = std::accumulate(v2.begin(), v2.end(), sum);
+double sum_separately(const std::vector<std::vector<double>>& v) {
+    double sum = 0.0;
+    for(size_t i=0; i<v.size(); i++) {
+        sum = std::accumulate(v[i].begin(), v[i].end(), sum);
+    }
     return sum;
 }
 
 
 
 std::vector<double> mk_vec(size_t size){
-    std::vector<double> v{size};
+    std::vector<double> v(size);
     std::generate(v.begin(), v.end(), [](){return dist(rnd);});
     return v;
 }
 
 
 static void BM_SumTogether(benchmark::State& state) {
-    std::vector<double> v1 = mk_vec(VEC_SZIE);
-    std::vector<double> v2 = mk_vec(VEC_SZIE);
+    std::vector<std::vector<double>> v;
+    for(size_t i=0; i<state.range(0); i++) {
+        v.push_back(mk_vec(VEC_SZIE));
+    }
     for (auto _ : state) {
-        auto sum = sum_together(v1, v2);
+        auto sum = sum_together(v);
         benchmark::DoNotOptimize(sum);
     }
 }
 
 
 static void BM_SumSeparately(benchmark::State& state) {
-    std::vector<double> v1 = mk_vec(VEC_SZIE);
-    std::vector<double> v2 = mk_vec(VEC_SZIE);
+    std::vector<std::vector<double>> v;
+    for(size_t i=0; i<state.range(0); i++) {
+        v.push_back(mk_vec(VEC_SZIE));
+    }
     for (auto _ : state) {
-        auto sum = sum_separately(v1, v2);
+        auto sum = sum_separately(v);
         benchmark::DoNotOptimize(sum);
     }
 }
 
 
-BENCHMARK(BM_SumTogether);
-BENCHMARK(BM_SumSeparately);
+BENCHMARK(BM_SumTogether)->RangeMultiplier(2)->Range(1, 10);
+BENCHMARK(BM_SumSeparately)->RangeMultiplier(2)->Range(1, 10);
 
 BENCHMARK_MAIN();
