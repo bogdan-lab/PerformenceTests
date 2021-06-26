123d64f3d31c5a9acbf0ae471073a0d7eb18d891
bogdan-lab <lakatosh@phystech.edu>
2021-06-26 21:05:36 +0300

    Preparing to testing different loggers

---
+++ PlayingWithLogger/Compare_Glog_and_Spdlog/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <QApplication>
+
+#include "log_widget.h"
+
+int main(int argc, char *argv[]) {
+  QApplication a(argc, argv);
+  LogWidget logObj;
+  logObj.AddText("Hello World!\n");
+  logObj << "IntValues = " << 1 << " ; " << 2 << '\n';
+  logObj << "DoubleValues = " << 3.14 << " ; " << 9.8 << '\n';
+  logObj.show();
+  return a.exec();
+}
