a7c193bab8b8f053310861e2083df591d9c268ac
bogdan-lab <lakatosh@phystech.edu>
2021-06-26 22:17:12 +0300

    somehow working error handler

--- PlayingWithLogger/Compare_Glog_and_Spdlog/main.cpp
+++ PlayingWithLogger/Compare_Glog_and_Spdlog/main.cpp
@@ -1,8 +1,9 @@
 ﻿#include <QApplication>
 
 #include "log_widget.h"
+#include "spdlog/spdlog.h"
 
-int main(int argc, char *argv[]) {
+int RunLogger(int argc, char* argv[]) {
   QApplication a(argc, argv);
   LogWidget logObj;
   logObj.AddText("Hello World!\n");
@@ -10,4 +11,20 @@ int main(int argc, char *argv[]) {
   logObj << "DoubleValues = " << 3.14 << " ; " << 9.8 << '\n';
   logObj.show();
   return a.exec();
+}
+
+#include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/spdlog.h"
+
+int main(int argc, char* argv[]) {
+  auto logger = spdlog::basic_logger_mt("basic_logger", "logs/basic-log.txt");
+  auto console = spdlog::stdout_color_mt("console");
+  spdlog::set_error_handler([](const std::string& msg) {
+    spdlog::get("basic_logger")->error("THIS IS ERROR MSG {}", msg);
+  });
+  spdlog::get("console")->info(
+      "some invalid message to trigger an error {}{}{}{}", 3);
+
+  return 0;
 }
