fe8e2d97596d02008ddd0b3325b82538a976d9a3
bogdan-lab <lakatosh@phystech.edu>
2021-06-26 22:23:09 +0300

    cleanup

--- PlayingWithLogger/Compare_Glog_and_Spdlog/main.cpp
+++ PlayingWithLogger/Compare_Glog_and_Spdlog/main.cpp
@@ -1,6 +1,8 @@
 ﻿#include <QApplication>
 
 #include "log_widget.h"
+#include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
 #include "spdlog/spdlog.h"
 
 int RunLogger(int argc, char* argv[]) {
@@ -13,18 +15,22 @@ int RunLogger(int argc, char* argv[]) {
   return a.exec();
 }
 
-#include "spdlog/sinks/basic_file_sink.h"
-#include "spdlog/sinks/stdout_color_sinks.h"
-#include "spdlog/spdlog.h"
+auto logger = spdlog::basic_logger_mt("basic_logger", "logs/basic-log.txt");
 
-int main(int argc, char* argv[]) {
-  auto logger = spdlog::basic_logger_mt("basic_logger", "logs/basic-log.txt");
+void error_handle_example() {
   auto console = spdlog::stdout_color_mt("console");
   spdlog::set_error_handler([](const std::string& msg) {
     spdlog::get("basic_logger")->error("THIS IS ERROR MSG {}", msg);
   });
-  spdlog::get("console")->info(
-      "some invalid message to trigger an error {}{}{}{}", 3);
+  console->info("some invalid message to trigger an error {}{}{}{}", 3);
+  int x = 10;
+  while (x--) {
+    console->info("I continue work {}", x);
+  }
+}
+
+int main(int argc, char* argv[]) {
+  error_handle_example();
 
   return 0;
 }
