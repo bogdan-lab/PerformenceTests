df95a7e7b3dc38281504c46432ac27890844d98b
bogdan-lab <lakatosh@phystech.edu>
2021-06-27 13:45:46 +0300

    cleanup and add glog test

--- PlayingWithLogger/Compare_Glog_and_Spdlog/main.cpp
+++ PlayingWithLogger/Compare_Glog_and_Spdlog/main.cpp
@@ -1,5 +1,4 @@
-﻿#include <QApplication>
-#include <chrono>
+﻿#include <chrono>
 #include <ctime>
 #include <iostream>
 #include <random>
@@ -7,49 +6,22 @@
 #include <vector>
 
 #include "glog/logging.h"
-#include "log_widget.h"
 #include "spdlog/sinks/basic_file_sink.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
 #include "spdlog/spdlog.h"
-
-int RunLogger(int argc, char* argv[]) {
-  QApplication a(argc, argv);
-  LogWidget logObj;
-  logObj.AddText("Hello World!\n");
-  logObj << "IntValues = " << 1 << " ; " << 2 << '\n';
-  logObj << "DoubleValues = " << 3.14 << " ; " << 9.8 << '\n';
-  logObj.show();
-  return a.exec();
-}
-/*
-auto logger = spdlog::basic_logger_mt("basic_logger", "logs/basic-log.txt");
-
-void error_handle_example() {
-  auto console = spdlog::stdout_color_mt("console");
-  spdlog::set_error_handler([](const std::string& msg) {
-    spdlog::get("basic_logger")->error("THIS IS ERROR MSG {}", msg);
-  });
-  console->info("some invalid message to trigger an error {}{}{}{}", 3);
-  int x = 10;
-  while (x--) {
-    console->info("I continue working {}", x);
-  }
-}
-*/
 
 int64_t LogSpdLog(const char* fname, size_t num) {
   std::mt19937 rnd{time(0)};
   std::uniform_real_distribution<double> dist_dbl{-1e4, 1e4};
   std::uniform_int_distribution<int> dist_int{-10000, 10000};
   int rnd_int = dist_int(rnd);
-  int rnd_dbl = dist_dbl(rnd);
-  spdlog::set_pattern("%v");
+  double rnd_dbl = dist_dbl(rnd);
   auto logger = spdlog::basic_logger_st("logger", fname, true);
   auto start = std::chrono::system_clock::now();
   while (num--) {
     logger->info(
         "This is long log message since I want to be clear. int = {}, double = "
-        "{}\n",
+        "{}",
         rnd_int, rnd_dbl);
   }
   auto end = std::chrono::system_clock::now();
@@ -82,10 +54,39 @@ void DoSpdLogTest(size_t log_num, size_t test_num) {
   std::cout << "SPDLOG = " << spd_avg << "ms +- " << spd_std << "ms\n";
 }
 
+int64_t LogGLog(size_t num) {
+  std::mt19937 rnd{time(0)};
+  std::uniform_real_distribution<double> dist_dbl{-1e4, 1e4};
+  std::uniform_int_distribution<int> dist_int{-10000, 10000};
+  int rnd_int = dist_int(rnd);
+  double rnd_dbl = dist_dbl(rnd);
+  auto start = std::chrono::system_clock::now();
+  while (num--) {
+    LOG(INFO) << "This is long log message since I want to be clear. int = "
+              << rnd_int << ", double = " << rnd_dbl;
+  }
+  auto end = std::chrono::system_clock::now();
+  auto duration =
+      std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+  return duration.count();
+}
+
+void DoGLogTest(size_t log_num, size_t test_num) {
+  FLAGS_log_dir = "glog_logs/";
+  google::InitGoogleLogging("GoogleLogger");
+  std::vector<int64_t> glog_res;
+  for (size_t i = 0; i < test_num; ++i) {
+    glog_res.push_back(LogGLog(log_num));
+  }
+  double glog_avg = get_avg(glog_res);
+  double glog_std = get_std(glog_res, glog_avg);
+  std::cout << "GLOG = " << glog_avg << "ms +- " << glog_std << "ms\n";
+}
+
 int main(int argc, char* argv[]) {
-  // error_handle_example();  // EXAMPLE FOR SPDLOG
   const size_t log_num = 1'000'000;
   const size_t test_num = 50;
   DoSpdLogTest(log_num, test_num);
+  DoGLogTest(log_num, test_num);
   return 0;
 }
