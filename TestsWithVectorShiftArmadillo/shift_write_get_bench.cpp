8f92b383de03fa1c5bd3ca4c67acd5ce4b9f92ea
bogdan-lab <lakatosh@phystech.edu>
2021-12-19 14:00:52 +0300

    added OnDemandArma case. No surprises. Sticking with in place case

--- TestsWithVectorShiftArmadillo/shift_write_get_bench.cpp
+++ TestsWithVectorShiftArmadillo/shift_write_get_bench.cpp
@@ -79,6 +79,39 @@ class ShiftOnDemand {
   size_t begin_idx_ = 0;
 };
 
+class ShiftOnDemandArma {
+ public:
+  ShiftOnDemandArma(const arma::vec& input) : data_(input) {}
+
+  void ShiftAndWrite(double val) {
+    data_[begin_idx_] = val;
+    begin_idx_ = Next(begin_idx_);
+  }
+
+  arma::vec& GetData() {
+    Flatten();
+    return data_;
+  }
+
+  double HackGetElement(uint64_t idx) const {
+    return data_[idx % data_.size()];
+  }
+
+ private:
+  void Flatten() {
+    data_ = arma::shift(data_, -begin_idx_);
+    begin_idx_ = 0;
+  }
+
+  int Next(int pos) const {
+    ++pos;
+    return pos == data_.size() ? 0 : pos;
+  }
+
+  arma::vec data_;
+  int begin_idx_ = 0;
+};
+
 static void BM_Current(benchmark::State& state) {
   std::mt19937 rnd(42);
   std::uniform_real_distribution<double> dist{
@@ -144,8 +177,31 @@ static void BM_ShiftOnDemand(benchmark::State& state) {
   }
 }
 
-BENCHMARK(BM_Current)->ArgsProduct({{50, 100, 200}, {1, 2, 5, 1000}});
-BENCHMARK(BM_InPlaceShift)->ArgsProduct({{50, 100, 200}, {1, 2, 5, 1000}});
-BENCHMARK(BM_ShiftOnDemand)->ArgsProduct({{50, 100, 200}, {1, 2, 5, 1000}});
+static void BM_ShiftOnDemandArma(benchmark::State& state) {
+  std::mt19937 rnd(42);
+  std::uniform_real_distribution<double> dist{
+      std::numeric_limits<double>::max(), std::numeric_limits<double>::min()};
+  arma::vec input(state.range(0));
+  std::generate(input.begin(), input.end(), [&]() { return dist(rnd); });
+
+  ShiftOnDemandArma latest_bars(std::move(input));
+
+  uint64_t request_count = state.range(1) > state.range(0)
+                               ? std::numeric_limits<uint64_t>::max()
+                               : state.range(1);
+  uint64_t index = 1;
+  for (auto _ : state) {
+    latest_bars.ShiftAndWrite(latest_bars.HackGetElement(index));
+    if (!(index % request_count)) {
+      benchmark::DoNotOptimize(latest_bars.GetData());
+    }
+    ++index;
+  }
+}
+
+BENCHMARK(BM_Current)->ArgsProduct({{50, 100, 200}, {1, 2, 5, 10}});
+BENCHMARK(BM_InPlaceShift)->ArgsProduct({{50, 100, 200}, {1, 2, 5, 10}});
+BENCHMARK(BM_ShiftOnDemand)->ArgsProduct({{50, 100, 200}, {1, 2, 5, 10}});
+BENCHMARK(BM_ShiftOnDemandArma)->ArgsProduct({{50, 100, 200}, {1, 2, 5, 10}});
 
 BENCHMARK_MAIN();
