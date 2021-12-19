013e3e4bbc00c3f0c45e45659d317c4b096665b4
bogdan-lab <lakatosh@phystech.edu>
2021-12-19 14:54:33 +0300

    cleanup

--- scripts/plot_benchmarks.py
+++ scripts/plot_benchmarks.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
+import argparse
 import json
 import numpy as np
 import re
 
 
-
 def check_name(name, regex):
-    if len(regex)==0:
+    if len(regex) == 0:
         return True
     if re.findall(regex, name):
         return True
@@ -32,38 +32,42 @@ def read_info(fname, regex):
     return data
 
 
+def setup_parser(parser):
+    parser.add_argument('-i', "--input", default="results.json",
+                        help="input json file with results")
+    parser.add_argument("--norm", action="store_true", default=False,
+                        help="Sets whether graph should be plotted using "
+                        "normalized times or not")
+    parser.add_argument("--figsize", action="store", default="7 4",
+                        help="Setting resultin figure size")
+    parser.add_argument("--filter", action="store", default="",
+                        help="Python regex for filtering which data to plot")
 
 
+def plot_data(data, args):
+    if args.norm:
+        data["cpu_t"] = data["cpu_t"]/min(data["cpu_t"])
+        data["real_t"] = data["real_t"]/min(data["real_t"])
+        data["t_unit"] = "a.u."
+    fig_size = args.figsize.split()
+    fig_size[0] = int(fig_size[0])
+    fig_size[1] = int(fig_size[1])
+    plt.figure(figsize=fig_size)
+    plt.grid(zorder=0)
+    plt.barh(data["names"], data["cpu_t"], zorder=3)
+    plt.xlabel("time, %s" % data["t_unit"])
+    plt.tight_layout()
+    plt.show()
 
 
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('-i', default="results.json",  help="input file [def = 'results.json']")
-parser.add_argument("--norm", action="store_false", default=True, help="Normalize all times to the smallest [def=True]")
-parser.add_argument("--real", action="store_true", default=False, help="Plot real time graph [def=False]")
-parser.add_argument("--figsize", action="store", default="7 4", help="Figsize parameter [def = '7 4']")
-parser.add_argument("--filter", action="store", default="", help="regex for peaking certain test names [def='' - take all]")
-args = parser.parse_args()
-
-fig_size = args.figsize.split()
-fig_size[0] = int(fig_size[0])
-fig_size[1] = int(fig_size[1])
-
-data = read_info(args.i, args.filter)
-
-if args.norm:
-    data["cpu_t"] = data["cpu_t"]/min(data["cpu_t"]) 
-    data["real_t"] = data["real_t"]/min(data["real_t"]) 
-    data["t_unit"] = "a.u."
-
-
-
-
-plt.figure(figsize=fig_size)
-plt.grid(zorder=0)
-plt.barh(data["names"], data["cpu_t"], zorder=3)
-plt.xlabel("time, %s" % data["t_unit"])
-plt.tight_layout()
-plt.show()
-
-
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+            prog="Benchmark results plotter",
+            description="Tool for selective plotting benchmark results from"
+            " json file",
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter
+            )
+    setup_parser(parser)
+    args = parser.parse_args()
+    data = read_info(args.input, args.filter)
+    plot_data(data, args)
