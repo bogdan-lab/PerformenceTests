628fdf4d9187d4e30c9445e18a53fa0d6ab4d5b3
bogdan-lab <lakatosh@phystech.edu>
2021-12-19 16:42:12 +0300

    grouping as I want

--- scripts/plot_benchmarks.py
+++ scripts/plot_benchmarks.py
@@ -10,21 +10,18 @@ def read_info(fname, regex):
     regex = re.compile(regex)
     with open(fname, "r") as f:
         json_data = json.load(f)
-    data = {"benchmarks": defaultdict(lambda: defaultdict(list))}
+    data = []
     all_time_units = set()
     for b_mark in json_data["benchmarks"]:
         if regex.search(b_mark["name"]):
-            group_name = b_mark["name"].split('/')[0]
-            data["benchmarks"][group_name]["names"].append(b_mark["name"])
-            data["benchmarks"][group_name]["cpu_time"].append(
-                    b_mark["cpu_time"])
+            data.append(
+                    {"name": b_mark["name"],
+                     "cpu_time": b_mark["cpu_time"],
+                     "time_unit": b_mark["time_unit"]}
+                    )
             all_time_units.add(b_mark["time_unit"])
-    for k in data["benchmarks"]:
-        data["benchmarks"][k]["cpu_time"] = np.array(
-                data["benchmarks"][k]["cpu_time"])
     if len(all_time_units) > 1:
         raise RuntimeError("There are different time units in the file")
-    data["time_unit"] = all_time_units.pop()
     return data
 
 
@@ -42,21 +39,51 @@ def setup_parser(parser):
 
 def normalize_times(data):
     min_val = np.inf
-    for key, val in data["benchmarks"].items():
-        min_val = min(min_val, min(val["cpu_time"]))
-    for k in data["benchmarks"]:
-        data["benchmarks"][k]["cpu_time"]/min_val
-    data["time_unit"] = "a. u."
+    for el in data:
+        min_val = min(min_val, el["cpu_time"])
+    for i in range(len(data)):
+        data[i]["cpu_time"] /= min_val
+        data[i]["time_unit"] = "a.u."
+
+
+def get_group_name(name, index):
+    return name.split('/')[index]
+
+
+def check_separate_groups(data, idx):
+    groups = set()
+    for el in data:
+        groups.add(get_group_name(el["name"], idx))
+    return len(groups) > 1
+
+
+def find_separating_index(data):
+    min_slash_num = min([el["name"].count('/') for el in data])
+    for idx in range(0, min_slash_num+1):
+        if check_separate_groups(data, idx):
+            break
+    return idx
+
+
+def group_data(data):
+    sep_idx = find_separating_index(data)
+    result = defaultdict(lambda: defaultdict(list))
+    for el in data:
+        group_name = get_group_name(el["name"], sep_idx)
+        result[group_name]["names"].append(el["name"])
+        result[group_name]["cpu_time"].append(el["cpu_time"])
+    return result
 
 
 def plot_data(data, args):
     if args.norm:
         normalize_times(data)
+    groups = group_data(data)
     plt.figure(figsize=[int(val) for val in args.figsize.split()])
     plt.grid(zorder=0)
-    for key, val in data["benchmarks"].items():
+    for key, val in groups.items():
         plt.barh(val["names"], val["cpu_time"], zorder=3)
-    plt.xlabel(f"time, {data['time_unit']}")
+    plt.xlabel(f"time, {data[0]['time_unit']}")
     plt.tight_layout()
     plt.show()
 
