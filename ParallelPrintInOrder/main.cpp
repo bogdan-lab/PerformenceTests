550010e80f49ebefc394282242fdedbbf1ddd908
bogdan-lab <lakatosh@phystech.edu>
2022-04-07 23:36:57 +0300

    Playing with strict order execution in parallel
    Seems to be a stupid task, since in current task everything is done
    sequentially in two threads. But we still can look at it as at some
    estimation od the thread switching using different tqchnique ?
    Ot maybe later the task will be changed so there will be some work done
    in parallel.

---
+++ ParallelPrintInOrder/main.cpp
@@ -0,0 +1,110 @@
+﻿#include <benchmark/benchmark.h>
+
+#include <condition_variable>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <vector>
+
+class FooBarCV {
+ private:
+  const int num_;
+  std::mutex m_;
+  std::condition_variable cv_;
+  bool need_foo_ = true;
+  std::vector<std::string> data_;
+
+ public:
+  FooBarCV(int n) : num_(n) { data_.reserve(num_); }
+
+  const std::vector<std::string>& GetData() const { return data_; }
+
+  void WaitPrintFoo() {
+    std::unique_lock<std::mutex> lock(m_);
+    cv_.wait(lock, [this]() { return !need_foo_; });
+  }
+
+  void foo() {
+    for (int i = 0; i < num_; i++) {
+      std::unique_lock<std::mutex> lock(m_);
+      cv_.wait(lock, [this]() { return need_foo_; });
+      data_.push_back("foo");
+      need_foo_ = false;
+      cv_.notify_one();
+    }
+  }
+
+  void bar() {
+    WaitPrintFoo();
+    for (int i = 0; i < num_; i++) {
+      std::unique_lock<std::mutex> lock(m_);
+      cv_.wait(lock, [this]() { return !need_foo_; });
+      data_.back().append("bar");
+      need_foo_ = true;
+      cv_.notify_one();
+    }
+  }
+};
+
+class FooBarDoubleMutex {
+ private:
+  const int num_;
+  std::mutex bar_mutex_;
+  std::mutex foo_mutex_;
+  std::vector<std::string> data_;
+
+ public:
+  FooBarDoubleMutex(int n) : num_(n) {
+    data_.reserve(num_);
+    bar_mutex_.lock();
+  }
+
+  const std::vector<std::string>& GetData() const { return data_; }
+
+  void foo() {
+    for (int i = 0; i < num_; i++) {
+      foo_mutex_.lock();
+      data_.push_back("foo");
+      bar_mutex_.unlock();
+    }
+  }
+
+  void bar() {
+    for (int i = 0; i < num_; i++) {
+      bar_mutex_.lock();
+      data_.back().append("bar");
+      foo_mutex_.unlock();
+    }
+  }
+};
+
+static void BM_ConVarThread(benchmark::State& state) {
+  for (auto _ : state) {
+    FooBarCV test(state.range(0));
+    auto lhs = std::thread([&test]() { test.foo(); });
+    auto rhs = std::thread([&test]() { test.bar(); });
+    lhs.join();
+    rhs.join();
+    benchmark::DoNotOptimize(test.GetData());
+  }
+}
+
+static void BM_DoubleMutexThread(benchmark::State& state) {
+  for (auto _ : state) {
+    FooBarDoubleMutex test(state.range(0));
+    auto lhs = std::thread([&test]() { test.foo(); });
+    auto rhs = std::thread([&test]() { test.bar(); });
+    lhs.join();
+    rhs.join();
+    benchmark::DoNotOptimize(test.GetData());
+  }
+}
+
+BENCHMARK(BM_ConVarThread)->Range(10, 10 << 10);
+BENCHMARK(BM_DoubleMutexThread)->Range(10, 10 << 10);
+
+// TODO what is semaphore?
+// TODO try multithread realization va=ia std::async
+// TODO try what happens, when we have tasks, which takesome time - sounds
+// stupid, anyway because of the strict order of execution current task will
+// have to operate sequentially!
