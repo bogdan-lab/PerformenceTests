7ff66972a9affd32bd883692773861bdc3249996
bogdan-lab <lakatosh@phystech.edu>
2022-08-01 23:05:40 +0300

    added sorted vector solution benchmark

--- MultiChannelMerge/main.cpp
+++ MultiChannelMerge/main.cpp
@@ -1,4 +1,5 @@
 ﻿#include <algorithm>
+#include <ctime>
 #include <deque>
 #include <queue>
 #include <random>
@@ -8,7 +9,7 @@
 #include "benchmark/benchmark.h"
 
 std::vector<std::queue<double>> GenerateSources(int src_num, int src_len) {
-  std::mt19937 rnd;
+  std::mt19937 rnd(std::time(nullptr));
   std::uniform_real_distribution<double> dist(0.0, 1.0);
   std::vector<std::queue<double>> result;
   result.reserve(src_num);
@@ -110,10 +111,52 @@ static void HeapSolutionBM(benchmark::State& state) {
   }
 }
 
+std::vector<double> SolveUsingSortedVector(
+    std::vector<std::queue<double>>& data) {
+  std::vector<double> result;
+  result.reserve(data.size() * data[0].size());
+  std::vector<std::queue<double>*> buff;
+  buff.reserve(data.size());
+  for (auto& el : data) {
+    buff.push_back(&el);
+  }
+  auto comp = [](const std::queue<double>* lhs, const std::queue<double>* rhs) {
+    return lhs->front() > rhs->front();
+  };
+  std::sort(buff.begin(), buff.end(), comp);
+
+  while (!buff.empty()) {
+    auto* top = buff.back();
+    buff.pop_back();
+    result.push_back(top->front());
+    top->pop();
+    if (!top->empty()) {
+      auto it = std::lower_bound(buff.begin(), buff.end(), top, comp);
+      buff.insert(it, top);
+    }
+  }
+
+  return result;
+}
+
+static void SortVectorSolutionBM(benchmark::State& state) {
+  for (auto _ : state) {
+    auto sources =
+        GenerateSources(/*src_num=*/state.range(0), /*src_len=*/state.range(1));
+    benchmark::DoNotOptimize(sources);
+    std::vector<double> res = SolveUsingSortedVector(sources);
+    benchmark::DoNotOptimize(res);
+  }
+}
+
 // Register the function as a benchmark
-BENCHMARK(GenerateSourceBM)->ArgsProduct({{10, 100, 1000}, {10, 100, 1000}});
-BENCHMARK(SetSolutionBM)->ArgsProduct({{10, 100, 1000}, {10, 100, 1000}});
-BENCHMARK(HeapSolutionBM)->ArgsProduct({{10, 100, 1000}, {10, 100, 1000}});
+BENCHMARK(GenerateSourceBM)
+    ->ArgsProduct({{1, 10, 100, 1000}, {1, 10, 100, 1000}});
+BENCHMARK(SetSolutionBM)->ArgsProduct({{1, 10, 100, 1000}, {1, 10, 100, 1000}});
+BENCHMARK(HeapSolutionBM)
+    ->ArgsProduct({{1, 10, 100, 1000}, {1, 10, 100, 1000}});
+BENCHMARK(SortVectorSolutionBM)
+    ->ArgsProduct({{1, 10, 100, 1000}, {1, 10, 100, 1000}});
 
 // Run the benchmark
 BENCHMARK_MAIN();
