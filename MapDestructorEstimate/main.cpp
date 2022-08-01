8645c24331ee1e666cf9a3f390ed1706546b5bc3
bogdan-lab <lakatosh@phystech.edu>
2022-08-01 21:17:39 +0300

    Add benchmark for vector destruction

--- MapDestructorEstimate/main.cpp
+++ MapDestructorEstimate/main.cpp
@@ -1,6 +1,7 @@
 ﻿#include <deque>
 #include <map>
 #include <random>
+#include <utility>
 
 #include "benchmark/benchmark.h"
 
@@ -34,7 +35,7 @@ void DestroyRow(DataRow& data) {
   std::swap(tmp, data);
 }
 
-static void DestructionBM(benchmark::State& state) {
+static void MapDestructionBM(benchmark::State& state) {
   for (auto _ : state) {
     state.PauseTiming();
     DataRow row =
@@ -46,9 +47,52 @@ static void DestructionBM(benchmark::State& state) {
   }
 }
 
+struct VecPoint {
+  time_t timestamp = 0;
+  double value = 0.0;
+  std::vector<std::pair<int, double>> details;
+};
+
+using DataRowVec = std::deque<VecPoint>;
+
+DataRowVec GenerateRowVec(int row_len, int detail_len) {
+  std::mt19937 rnd(42);
+  std::uniform_real_distribution<double> r_dist(0, 1);
+  std::uniform_int_distribution<int> i_dist(0, 100000);
+  DataRowVec result;
+  while (row_len--) {
+    result.emplace_back();
+    result.back().timestamp = i_dist(rnd);
+    result.back().details.reserve(detail_len);
+    for (int i = 0; i < detail_len; ++i) {
+      double val = r_dist(rnd);
+      result.back().details.emplace_back(i, val);
+      result.back().value += val;
+    }
+  }
+  return result;
+}
+
+void DestroyRowVec(DataRowVec& data) {
+  DataRowVec tmp;
+  std::swap(tmp, data);
+}
+
+static void VecDestructionBM(benchmark::State& state) {
+  for (auto _ : state) {
+    state.PauseTiming();
+    DataRowVec row = GenerateRowVec(/*row_len=*/state.range(0),
+                                    /*detail_len=*/state.range(1));
+    state.ResumeTiming();
+    benchmark::DoNotOptimize(row);
+    DestroyRowVec(row);
+    benchmark::DoNotOptimize(row);
+  }
+}
+
 // Register the function as a benchmark
-BENCHMARK(DestructionBM)
-    ->ArgsProduct({{10, 100, 1000, 10000}, {10, 100, 1000, 10000}});
+BENCHMARK(MapDestructionBM)->ArgsProduct({{10, 100, 1000}, {10, 100, 1000}});
+BENCHMARK(VecDestructionBM)->ArgsProduct({{10, 100, 1000}, {10, 100, 1000}});
 
 // Run the benchmark
 BENCHMARK_MAIN();
